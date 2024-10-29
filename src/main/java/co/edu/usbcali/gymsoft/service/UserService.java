264d3c431a25f512c3352e9a5bb8d828c59744c6
Jesus David Mejia Vergara <davidmejia-2000@outlook.com>
2024-10-28 20:17:12 -0500

    Service y Rest del User

---
+++ src/main/java/co/edu/usbcali/gymsoft/service/UserService.java
@@ -0,0 +1,32 @@
+package co.edu.usbcali.gymsoft.service;
+
+import co.edu.usbcali.gymsoft.domain.User;
+import co.edu.usbcali.gymsoft.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    public UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getById(int userId){
+        return this.userRepository.getById(userId);
+    }
+
+    public List<User> getAll() {
+        return this.userRepository.findAll();
+    }
+
+    public User save(User user) {
+        return this.userRepository.save(user);
+    }
+
+}
