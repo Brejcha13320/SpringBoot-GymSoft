558196b7230761b67235c3944964b2d093e89427
SATRACK\Gustavo_Navarro <Gustavo_Navarro@satrack.com>
2024-10-31 22:54:28 -0500

    get user by id

--- src/main/java/co/edu/usbcali/gymsoft/service/UserService.java
+++ src/main/java/co/edu/usbcali/gymsoft/service/UserService.java
@@ -3,7 +3,9 @@ package co.edu.usbcali.gymsoft.service;
 import co.edu.usbcali.gymsoft.domain.User;
 import co.edu.usbcali.gymsoft.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -18,7 +20,8 @@ public class UserService {
     }
 
     public User getById(int userId){
-        return this.userRepository.getById(userId);
+        return this.userRepository.findById(userId).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
     }
 
     public List<User> getAll() {
