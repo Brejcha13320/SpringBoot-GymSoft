a783f8870cd2d61e2978cdc0b736b0c6ec307884
SATRACK\Gustavo_Navarro <Gustavo_Navarro@satrack.com>
2024-11-11 16:40:28 -0500

    clean code

--- src/main/java/co/edu/usbcali/gymsoft/service/ClientMembershipService.java
+++ src/main/java/co/edu/usbcali/gymsoft/service/ClientMembershipService.java
@@ -1,7 +1,6 @@
 package co.edu.usbcali.gymsoft.service;
 
 import co.edu.usbcali.gymsoft.dto.ClientMembershipDTO;
-import co.edu.usbcali.gymsoft.dto.MembershipDTO;
 import co.edu.usbcali.gymsoft.dto.request.CreateClientMembershipRequest;
 
 import java.util.List;
--- src/main/java/co/edu/usbcali/gymsoft/web/ClientMembershipController.java
+++ src/main/java/co/edu/usbcali/gymsoft/web/ClientMembershipController.java
@@ -1,7 +1,6 @@
 package co.edu.usbcali.gymsoft.web;
 
 import co.edu.usbcali.gymsoft.dto.ClientMembershipDTO;
-import co.edu.usbcali.gymsoft.dto.MembershipDTO;
 import co.edu.usbcali.gymsoft.dto.request.CreateClientMembershipRequest;
 import co.edu.usbcali.gymsoft.service.ClientMembershipService;
 import jakarta.validation.Valid;
@@ -40,20 +39,5 @@ public class ClientMembershipController {
         ClientMembershipDTO clientMembershipDTO = clientMembershipService.createClientMembership(createClientMembershipRequest);
         return ResponseEntity.ok().body(clientMembershipDTO);
     }
-
-//    @GetMapping
-//    public ResponseEntity<List<ClientMembership>> getAll(){
-//        return ResponseEntity.ok(this.clientMembershipService.getAll());
-//    }
-//
-//    @GetMapping("/{clientMembershipId}")
-//    public ResponseEntity<ClientMembership> getById(@PathVariable int clientMembershipId){
-//        return ResponseEntity.ok(this.clientMembershipService.getById(clientMembershipId));
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity<ClientMembership> save(@RequestBody ClientMembership clientMembership){
-//        return ResponseEntity.ok(this.clientMembershipService.save(clientMembership));
-//    }
 
 }
